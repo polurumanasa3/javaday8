83fad2af09d58b9671937b96b9fa320afd150fe8
polurumanasa3 <manasap142@gmail.com>
2024-12-09 02:06:47 -0800

    Add files via upload

---
+++ o.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class o
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        int c=sc.nextInt();
+        int arr[] []=new int[r][c];
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println("Before transpose");
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println("After transpose");
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++){
+                System.out.print(arr[j][i]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+            
+        
+            
+        
