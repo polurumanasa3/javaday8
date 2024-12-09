ebb0694a8f4829068412ee3a08ef1068d65bcb7c
polurumanasa3 <manasap142@gmail.com>
2024-12-09 02:41:03 -0800

    Add files via upload

--- a.java
+++ a.java
@@ -1,26 +1,13 @@
-import java.util.*;
+import java.util.Scanner; 
 class a
 {
-    public static void main(String args[])
-    {
-    Scanner sc=new Scanner(System.in);
-    int n=sc.nextInt();
-    int m=n;
-    int arr[]=new int[n];
-    for(int r=0;r<n;r++)
-    arr[r]=sc.nextInt();
-    int post[]=new int[m];
-    post[0]=arr[(n-1)];
-    m--;
-    for(int i=1;i<n;i++)
-    {
-        m--;
-    post[i]=post[(i-1)]+arr[m];
-    }
-    System.out.println(Arrays.toString(post));
-    }
+	public static void main (String[] args)
+	{
+	    // Do not print anything here before taking input
+	    Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		System.out.println(n);
+		
+	}
 }
-
-
-
-    
