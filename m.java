cf0b4d38719871300b241663be6361dcfb0261e4
polurumanasa3 <manasap142@gmail.com>
2024-12-09 01:33:18 -0800

    Add files via upload

---
+++ m.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class m
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        int c=sc.nextInt();
+        int arr[] []=new int[r][c];
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        int max=arr[0][0];
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+                
+            
+            {
+                if(arr[i][j]>max)
+                max=arr[i][j];
+                
+            }
+            
+        }
+        System.out.println(max);
+        }
+
+            }
+        
+            
+        
